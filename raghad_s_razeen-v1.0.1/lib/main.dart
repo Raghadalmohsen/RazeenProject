ab3f5ff46a52aae9598fd5f648b98b3ddb4e3d6b
Albatool0 <145212015+Albatool0@users.noreply.github.com>
2024-02-19 02:44:10 +0300

    ربط الداتابيس

--- raghad_s_razeen-v1.0.1/lib/main.dart
+++ raghad_s_razeen-v1.0.1/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart';
 import 'core/app_export.dart';
+import 'package:firebase_core/firebase_core.dart';/////////////
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-void main() {
+Future<void> main() async {/////////////
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();//////////
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
